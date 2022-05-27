6c36b6c0d801289639e2d3203847e55708bdfe41
Ra Wai UCSM <yawaithaethae133@gmail.com>
2022-05-27 04:43:28 -0700

    Updates

---
+++ precache-manifest.cfee8817b82e9e734749737c5d3c5d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a909063a0d63b25f7f9d6792f419846",
+    "url": "/Context-todo/index.html"
+  },
+  {
+    "revision": "9e2531a5a8fe5ea4e56a",
+    "url": "/Context-todo/static/css/main.77b0c7d8.chunk.css"
+  },
+  {
+    "revision": "2ae8e0eec233b1a0f2e6",
+    "url": "/Context-todo/static/js/2.cb2c4f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Context-todo/static/js/2.cb2c4f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2531a5a8fe5ea4e56a",
+    "url": "/Context-todo/static/js/main.225efcc9.chunk.js"
+  },
+  {
+    "revision": "dee8c3d052db4f228e98",
+    "url": "/Context-todo/static/js/runtime-main.95f5fe13.js"
+  }
+]);
